dc29f583c2701f2ba2e4fd42af93bc47044686ca
WoleEdwards <kinwole14@gmail.com>
2025-02-27 12:36:45 -0500

    Small Changes
    Made some changes to ultrasonic sensor test file.

--- ultrasonicSensorTest.py
+++ ultrasonicSensorTest.py
@@ -4,19 +4,22 @@ from time import sleep
 from robot_hat import Music,TTS
 import readchar
 
+#distance to maintain from obstacles in front
 safeDistance = 20
 
 def main():
 
     flag_bgm = False
     music.music_set_volume(20)
-    tts.lang("en-US")
-
-    safeDistance = 20
     
+    #constantly checkthe distance 
     while True:
+
+        #read the obstacle distance in front of the car
         distance = round(px.ultrasonic.read(), 2)
         print("distance: ", distance)
+
+        #check if the distance is liss than the safe distance
         if (distance < safeDistance):
             music.sound_play_threading('../sounds/car-double-horn.wav')
             sleep(0.05)
@@ -24,7 +27,8 @@ def main():
         else:
             print("Safe\n")
             
-        
-
+#run the main function
+if __name__ == "__main__":
+    main()
 
 
