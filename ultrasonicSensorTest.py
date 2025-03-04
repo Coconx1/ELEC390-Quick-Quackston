51df43667dda5b7f1f31d14fb76c1d3173367763
WoleEdwards <156554857+WoleEdwards@users.noreply.github.com>
2025-03-04 13:47:45 -0500

    added greyscale commands

--- ultrasonicSensorTest.py
+++ ultrasonicSensorTest.py
@@ -15,6 +15,11 @@ def main():
     #constantly checkthe distance 
     while True:
 
+        gm_val_list = px.get_grayscale_data()
+        gm_state = get_status(gm_val_list)
+        print("gm_val_list: %s, %s"%(gm_val_list, gm_state))
+         _state = px.get_line_status(val_list)
+
         #read the obstacle distance in front of the car
         distance = round(px.ultrasonic.read(), 2)
         print("distance: ", distance)
